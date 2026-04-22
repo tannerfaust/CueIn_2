9a44c90d2be5f0b34e0f723d6a0ebb47a2b27d20
tannerfaust <tannerwellspoken@proton.me>
2026-04-23 01:43:08 +0200

    Initial Commit

---
+++ CueIn_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CueIn_2
+//
+//  Created by Tanner Fause on 23.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CueIn_2/CueIn_2App.swift
@@ -0,0 +1,17 @@
+//
+//  CueIn_2App.swift
+//  CueIn_2
+//
+//  Created by Tanner Fause on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CueIn_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
